b2e565068721a15988a3a27381de687c31686005
tian <tianwu@outlook.com>
2022-02-20 22:37:49 +1100

    add title section & placeholders for footer & hero

---
+++ src/components/section/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Hero = () => {
+	return (
+		<>
+			<div></div>
+		</>
+	);
+};
+
+export default Hero;
+
+const Wrapper = styled.div``;
