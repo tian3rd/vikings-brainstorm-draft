44186933c5a22bb9208c1af9875dfc01b2f922c9
tian <tianwu@outlook.com>
2022-02-20 22:37:08 +1100

    add wave background

---
+++ src/components/background/Background.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import wave from "../../assets/images/Wave.svg";
+
+const Background = () => {
+	return (
+		<Wrapper>
+			<Wave src={wave} alt="wave" />
+		</Wrapper>
+	);
+};
+
+export default Background;
+
+const Wrapper = styled.div`
+	position: relative;
+	display: grid;
+	place-items: center;
+	width: 100%;
+	height: 200px;
+`;
+
+const Wave = styled.img`
+	position: absolute;
+	width: 100%;
+	height: 323.61px;
+	top: -150px;
+	z-index: 1;
+`;
