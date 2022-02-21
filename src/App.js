eaadf1ffbf4f12666346b7ad59e579440c2d7059
tian <tianwu@outlook.com>
2022-02-21 15:30:20 +1100

    move find solution in the main app to render only once per game

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import Layout from "./components/layout/layout";
 import Title from "./components/section/Title";
 import Board from "./components/board/Board";
 import Background from "./components/background/Background";
+import { findSolution } from "./components/board/boardLogic";
 
 function App() {
+	const solution = findSolution("O1N1N1N0N0O0N2O0N3BaGrRoYn", "BwGkRbYn");
+	console.log("Solution: ", solution);
 	return (
 		<Layout>
 			<Wrapper>
 				<Title title="Vikings" version="1.0.0" />
 				<BoardWrapper>
-					<Board board="O0O1N1N3O1N2N3N3N2BbYp" target="BdYn" />
+					{/* <Board board="O0O1N1N3O1N2N3N3N2BbYp" target="BdYn" /> */}
 					{/* <Board board="O0N1N1N2N1O0N2O0N2Be" target="Bc" /> */}
 					{/* <Board board="O1N1O0N3N1N2N3N3O1BlGh" target="BaGx" /> */}
 					{/* <Board board="O0O1O0N3N1N2N3N2N2Gs" target="Gu" /> */}
+					<Board board="O1N1N1N0N0O0N2O0N3BaGrRoYn" target="BwGkRbYn" />
 				</BoardWrapper>
 				<Background />
 			</Wrapper>
