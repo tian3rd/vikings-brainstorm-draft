bfb999f3ce3d156a315192ec693665f7e387cf9f
tian <tianwu@outlook.com>
2022-02-23 20:19:24 +1100

    adjust margin bottom

--- src/components/section/Title.js
+++ src/components/section/Title.js
@@ -2,40 +2,40 @@ import React from "react";
 import styled from "styled-components";
 
 const Title = ({ title, version }) => {
-	return (
-		<Wrapper>
-			<TitleWrapper>
-				{title}
-				<Version>{version}</Version>
-			</TitleWrapper>
-		</Wrapper>
-	);
+  return (
+    <Wrapper>
+      <TitleWrapper>
+        {title}
+        <Version>{version}</Version>
+      </TitleWrapper>
+    </Wrapper>
+  );
 };
 
 export default Title;
 
 const Wrapper = styled.div`
-	position: relative;
-	display: grid;
-	place-items: center;
-	padding: 10px;
-	margin: 0 auto 50px;
+  position: relative;
+  display: grid;
+  place-items: center;
+  padding: 10px;
+  margin: 0 auto 80px;
 `;
 
 const TitleWrapper = styled.h1`
-	display: flex;
-	font-family: Akronim;
-	font-style: normal;
-	font-weight: normal;
-	font-size: 96px;
-	line-height: 110px;
-	color: #000000;
+  display: flex;
+  font-family: Akronim;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 96px;
+  line-height: 110px;
+  color: #000000;
 `;
 
 const Version = styled.p`
-	position: absolute;
-	font-family: "SF Pro Text";
-	font-size: 12px;
-	top: -20px;
-	right: calc(50% - 150px);
+  position: absolute;
+  font-family: "SF Pro Text";
+  font-size: 12px;
+  top: -20px;
+  right: calc(50% - 150px);
 `;
